436cfb98c025862d1687502a245f0dea86e33eaa
Koti79k <koteswararao791212@gmail.com>
2022-10-05 11:36:51 +0530

    updated

--- index.js
+++ index.js
@@ -14,4 +14,29 @@ function checkBoxes(){
     })
 }
 
+// function darkMode() {
+// 		var element = document.body;
+// 		var content = document.getElementById("DarkModetext");
+// 		element.className = "dark-mode";
+// 		// content.innerText = "Dark Mode is ON";
+// 	}
+// 	function lightMode() {
+// 		var element = document.body;
+// 		var content = document.getElementById("DarkModetext");
+// 		element.className = "light-mode";
+// 		// content.innerText = "Dark Mode is OFF";
+// 	}
+
+function myFunction() {
+   var element = document.body;
+	element.classList.toggle("dark-mode");
+	
+}
+const btn = document.getElementById('moon');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'white';
+  btn.style.color = 'black';
+});
+
 
