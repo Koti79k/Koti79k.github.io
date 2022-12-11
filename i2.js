6f2cc2aa89689d416de58f94c0523977278bac5b
Koti79k <Koteswararao791212@gmail.com>
2022-12-11 20:05:35 +0530

    updated

---
+++ i2.js
@@ -0,0 +1,66 @@
+function Live5() {
+  window.open(
+    "https://drive.google.com/file/d/1hmbZqnAAEh5QozG4-CnATmA1Vw-bOrah/view?usp=share_link",
+    "_blank"
+  );
+}
+let pr3l = document.querySelector(".pr5").addEventListener("click", Live5);
+
+function Live6() {
+  window.open(
+    "https://github.com/Koti79k/Online-Cab-Booking-Application",
+    "_blank"
+  );
+}
+let pr32 = document.querySelector(".pr6").addEventListener("click", Live6);
+
+/* ========================================================================== */
+
+function Live7() {
+  window.open("https://reliable-llama-c038f5.netlify.app/", "_blank");
+}
+let pr33 = document.querySelector(".pr7").addEventListener("click", Live7);
+
+function Live8() {
+  window.open("https://github.com/thecodervaibhav/Orbitz.com-clone", "_blank");
+}
+let pr34 = document.querySelector(".pr8").addEventListener("click", Live8);
+
+/* ========================================================================== */
+
+function Live9() {
+  window.open("https://comfy-semolina-1501da.netlify.app/", "_blank");
+}
+let pr35 = document.querySelector(".pr9").addEventListener("click", Live9);
+
+function Live10() {
+  window.open("https://github.com/Koti79k/fatsecret", "_blank");
+}
+let pr36 = document.querySelector(".pr10").addEventListener("click", Live10);
+
+/* ========================================================================== */
+
+function Live11() {
+  window.open("https://super-empanada-c3011d.netlify.app/", "_blank");
+}
+let pr37 = document.querySelector(".pr11").addEventListener("click", Live11);
+
+function Live12() {
+  window.open("https://github.com/Koti79k/GeekBuying-U2", "_blank");
+}
+let pr38 = document.querySelector(".pr12").addEventListener("click", Live12);
+
+/* ========================================================================== */
+
+function Live13() {
+  window.open("https://stellar-marzipan-2c148d.netlify.app/", "_blank");
+}
+let pr39 = document.querySelector(".pr13").addEventListener("click", Live13);
+
+function Live14() {
+  window.open(
+    "https://github.com/masai-course/Kine_fw18_0279/tree/master/unit-3%3DJS201/sprint-3%3DYoutube%20App/day-1%3DYoutube/assignments/Youtube-2",
+    "_blank"
+  );
+}
+let pr40 = document.querySelector(".pr14").addEventListener("click", Live14);
